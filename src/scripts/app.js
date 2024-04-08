2de2c82875ae4f7752c95d2dea46af49e316649c
Alyona Rogovaya <alyona@yelk.com.ua>
2024-04-08 10:37:58 +0200

    Added a mobile menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
+const desktopSize = window.matchMedia("(min-width: 992px)");
+
 function toggleAppBanner() {
   const btnOpen = document.querySelector('.btn-open-banner')
   const btnClose = document.querySelector('.app-banner__close')
@@ -35,7 +37,34 @@ function createObserver() {
   observer.observe(boxElement);
 }
 
+function toggleMobileMenu() {
+  const openBtn = document.querySelector('.navbar__mobile-btn')
+  const overlay = document.querySelector(".navbar__overlay")
+  const header = document.querySelector(".navbar")
+
+  const closeMenu = () => {
+    header.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+  }
+
+  openBtn.addEventListener("click", () => {
+    header.classList.toggle("active");
+    document.body.classList.toggle("no-scroll")
+  })
+
+  overlay.addEventListener("click", () => {
+    closeMenu()
+  })
+
+  window.addEventListener("resize", () => {
+    if (desktopSize.matches) {
+      closeMenu()
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   toggleAppBanner();
-  createObserver()
+  createObserver();
+  toggleMobileMenu()
 })
